289cf2f9fdc1381d1eea13d6dd1ada67e862036d
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2015-09-07 20:46:06 +0100

    mas pruebas de cubrimiento

--- app/spec/app_spec.rb
+++ app/spec/app_spec.rb
@@ -3,7 +3,6 @@ require 'coveralls'
 Coveralls.wear!
 
 require_relative './../controllers/app.rb'
-require_relative "./../helpers/helpers.rb"
 ENV['RACK_ENV'] = 'test'
 
 require 'minitest/autorun'
@@ -35,6 +34,13 @@ describe "Test App: Check routes" do
 		assert last_response.body.include? 'Create a new account'
 	end
 
+	it "Check register" do
+		post '/register', :username => "test", :password => '1234', :email => "email@mail.com"
+		assert last_response.ok?
+		assert_equal "{\"control\":0}", last_response.body
+		User.first(:username => "test", :email => "email@mail.com").destroy
+	end
+
 	it "Check login" do
 		user = User.first_or_create(:username => "test", :email => "email@mail.com", :password => "1234")
 		post '/login',:name => user.username, :password => user.password
@@ -52,47 +58,17 @@ describe "Test App: Check routes" do
 		assert_equal "http://localhost:4567/logout", last_request.url.to_s
 	end
 
-end
-
-
-describe "Test Heleper functions" do
-
-	def app
-    	MyApp
+	it "Check recovery" do
+		user = User.first_or_create(:username => "test", :email => "email@mail.com", :password => "1234")
+		post '/recovery-account', :recovery_account => user.username
+		assert last_response.ok?
+		assert_equal "{\"control\":1}", last_response.body
+		user.destroy
 	end
 
-
-	it "Funtion: account_information" do
-	   assert_equal(Mail::Message,account_information("Username","1234","mail@email.com","Tests for coverlls").class)
-	end
-
-	it "Funtion: merma" do
-	   assert_equal merma(10, 50), 5
-	end
-
-	it "Funtion: GetIds" do
-	   assert_equal GetIds("Username_Recipe"), ["Username", "Recipe"]
-	end
-
-	it "Funtion: calculator1" do
-	   assert_equal calculator(25, 4, 2), 12
-	end
-
-	it "Funtion: calculator2" do
-	   assert_equal calculator(25, 2, 2), 25
-	end
-
-	it "Function: GenerateNewRecipeName" do
-		resul = false
-		cad = GenerateNewRecipeName("rice")
-		if (cad.include? "rice") and (cad.size == 8)
-			resul = true
-		end
-		assert(resul)
-	end
-
-	it "Funtion: to_bool" do
-		assert(to_bool("true")&&!to_bool("false"))
+	it "Check failiure route" do
+		get '/auth/failure'
+		expect(last_response).to be_ok
 	end
 
 end
